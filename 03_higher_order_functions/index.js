3adda709e8410cea86a09847afe4e980a5a52dad
ixnp <enprocopios@gmail.com>
2021-05-19 07:41:24 -0700

    higher order functions starter

---
+++ 03_higher_order_functions/index.js
@@ -0,0 +1,34 @@
+const books = [
+    {
+        title: 'Eloquent JavaScript',
+        price: 16.00,
+        inventory: 5,
+        reviews : [{user_id:1, content:'loved it'}, {user_id:5, content:'hard to understand for beginners'}]
+    },
+    {
+        title: 'JavaScript: The Good Parts',
+        price: 10.50,
+        inventory: 10,
+        reviews : [{user_id:110, content:'disagreed with everything in this book'}, {user_id:1, content:'I live and die by the words of this author'}]
+    },
+    {
+        title: 'Learn JavaScript VISUALLY',
+        price: 25.00,
+        inventory: 2,
+        reviews : [{user_id:11, content:'This really help me understand JavaScript'}]
+
+    },
+    {
+        title: 'You don\'t know js',
+        price: 60.00,
+        inventory: 8,
+        reviews : [{user_id:15, content:'I really didn\'t know js'},{user_id:19, content:'a little over my head at first but was really helpful about 6 months into my career'}]
+
+    }, {
+        title: 'JavaScript: The Definitive Guide',
+        price: 18.95,
+        inventory: 0,
+        reviews : [{user_id:21, content:'The most helpful guide to new developers'}]
+
+    }
+]
