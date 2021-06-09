cf28124cd23bcacb0be623278ff18561a640050c
Louis Michael Medina <louis@Louis-Medinas-MacBook-Pro.local>
2021-06-09 08:20:37 -0400

    add components & props solutions

---
+++ phase-2/solutions/components-and-props-morning/components-and-props-test/src/Header.js
@@ -0,0 +1,59 @@
+// Title Component
+
+function Title() {
+    return (
+        <div>
+            <h2>Here's my Title Component</h2>
+        </div>
+    );
+}
+
+// Form Component
+
+function Form() {
+    return (
+        <div>
+            <h2>Here's my Form Component</h2>
+        </div>
+    );
+}
+
+// Create a Header Component to display a Title
+function Header(props) {
+    // console.log(props);
+
+    // "isLoggedIn"
+
+    // Ternary Operator
+    // Condition ? ifTrue : ifFalse
+
+    // Think about using embedded JS in your JSX.
+
+    return (
+        <div>
+            <h3>{ props.isLoggedIn ? "Logged In" : "Logged Out" }</h3>
+            <h1>{props.title}</h1>
+            <h1>{props.name}</h1>
+        </div>
+    )
+
+    // if (props.isLoggedIn) {
+    //     return (
+    //         <div>
+    //             <h1>Logged In</h1>
+    //             <h1>{props.title}</h1>
+    //             <h2>{props.name}</h2>
+    //         </div>
+    //     );
+    // } else {
+    //     return (
+    //         <div>
+    //             <h1>Not Logged In</h1>
+    //             <h1>{props.title}</h1>
+    //             <h2>{props.name}</h2>
+    //         </div>
+    //     );
+    // }   
+}
+
+export { Header, Title, Form };
